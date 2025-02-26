0b66b7b28e5c433129aab13d95d627cefab7aafe
arghyacodes <arghyaghoshal.juetce@gmail.com>
2025-02-26 12:31:05 +0530

    updated code

--- Java26022025/HelloWorld.java
+++ Java26022025/HelloWorld.java
@@ -18,13 +18,6 @@ class HelloWorld
         System.out.println(ch);
 
     }
-    // public static void display(String[] args)
-    // {
-    //     System.out.println(a);
-    //     System.out.println(b);
-    //     System.out.println(c);
-    //     System.out.println(d);
-    // }
 }
 
 /*
