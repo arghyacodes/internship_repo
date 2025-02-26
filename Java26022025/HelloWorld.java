c3c753ad3eb7eb08690465bb481746e46cc373b3
arghyacodes <arghyaghoshal.juetce@gmail.com>
2025-02-26 12:22:58 +0530

    modif

--- Java26022025/HelloWorld.java
+++ Java26022025/HelloWorld.java
@@ -1,7 +1,49 @@
 class HelloWorld
 {
+    static int a;
+    static double b;
+    static boolean c;
+    static char d;
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
     }
+    public static void localv()
+    {
+        int j = 9;
+        double k = 9.909;
+        boolean b = false;
+        char ch = 'a';
+
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(b);
+        System.out.println(ch);
+    }
+
+    // public static void display()
+    // {
+    //     System.out.println(a);
+    //     System.out.println(b);
+    //     System.out.println(c);
+    //     System.out.println(d);
+    // }
 }
+
+/*
+ * Java Variables-
+ * Local, Global, Static, non-static/instance variables
+ */
+
+ /*
+  * Types of Data types 
+  Premiive - 
+  (int, boolean, float,long, double, short, byte, char)
+  non-primitive
+  (arrays, derived data types or user defined data types)
+  */
+
+  /*
+   * global -
+   * boolean = false
+   */
