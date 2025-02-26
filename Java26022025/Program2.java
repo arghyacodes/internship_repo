af00b00728c3401c4e6be39faaf797c7ac1afd6e
arghyacodes <arghyaghoshal.juetce@gmail.com>
2025-02-26 13:50:14 +0530

    updated code

---
+++ Java26022025/Program2.java
@@ -0,0 +1,11 @@
+public class Program2 {
+    public static void main(String[] args)
+    {
+        disp();
+    }
+    public static void disp()
+    {
+        int a = 10;
+        System.out.println(a);
+    }
+}
