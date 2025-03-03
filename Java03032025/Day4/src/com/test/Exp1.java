c886edfe4e550da717ebd9d2e851b97371a3ddf3
arghyacodes <arghyaghoshal.juetce@gmail.com>
2025-03-03 16:52:18 +0530

    3rd March

--- Java03032025/Day4/src/com/test/Exp1.java
+++ Java03032025/Day4/src/com/test/Exp1.java
@@ -1,5 +1,28 @@
 package com.test;
 
-public class Exp1 {
+import java.util.*;
+import java.util.stream.Collectors;
 
+class Employee {
+	private
+		int id;
+		String name;
+		int age;
+		String gender;
+		int yearOfJoining;
+		double salary;
+		
+	public Employee(int id, String name, int age, String gender, int yearOfJoining, double salary) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+	}
+}
+
+
+public class Exp1{
+	
 }
